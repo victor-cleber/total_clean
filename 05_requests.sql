7b9f63d5bee94b696c4aef5398e0d3fe56313052
victor-cleber <13708226+victor-cleber@users.noreply.github.com>
2023-10-18 10:16:05 +1300

    Add requests table

---
+++ 05_requests.sql
@@ -0,0 +1,11 @@
+CREATE TABLE requests (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    quantity INTEGER,
+    cod_cleaning INTEGER,
+    cod_item INTEGER,
+    status BOOLEAN,
+    deleted_at DATETIME,
+
+    FOREIGN KEY(cod_cleaning)) REFERENCES cleaning(id),
+    FOREIGN KEY(cod_item)) REFERENCES items(id)
+);
