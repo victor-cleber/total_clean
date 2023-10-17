64f655477ed62ee154e0389c3b044078c596b30e
victor-cleber <13708226+victor-cleber@users.noreply.github.com>
2023-10-17 15:55:25 +1300

    Items and items_type tables

---
+++ 02_item.sql
@@ -0,0 +1,19 @@
+CREATE TABLE items (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    quantity INTEGER,
+    description VARCHAR (155),
+    status BOOLEAN,
+    deleted_at DATETIME 
+);
+
+
+CREATE TABLE item_type (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(16),
+    description VARCHAR (100),
+    status BOOLEAN,
+    deleted_at DATETIME 
+    cod_item INTEGER,
+
+    FOREIGN KEY(cod_item)) REFERENCES item(id)
+);
