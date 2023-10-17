485d3e209daf8d0a2e3da62b4708d1b47253a112
victor-cleber <13708226+victor-cleber@users.noreply.github.com>
2023-10-18 10:20:49 +1300

    Update sql code

--- sql/05_requests.sql
+++ sql/05_requests.sql
@@ -6,6 +6,6 @@ CREATE TABLE requests (
     status BOOLEAN,
     deleted_at DATETIME,
 
-    FOREIGN KEY(cod_cleaning)) REFERENCES cleaning(id),
+    FOREIGN KEY(cod_cleaning)) REFERENCES cleanings(id),
     FOREIGN KEY(cod_item)) REFERENCES items(id)
 );
