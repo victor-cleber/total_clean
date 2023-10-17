21d2f8af413357a91366854693c3959784d7ad55
victor-cleber <13708226+victor-cleber@users.noreply.github.com>
2023-10-18 10:11:00 +1300

    Add cleaning table

---
+++ 04_cleaning.sql
@@ -0,0 +1,13 @@
+CREATE TABLE cleaners (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date DATETIME,
+    check_in DATETIME,
+    check_out DATETIME,
+    cod_cleaner INTEGER,
+    cod_room INTEGER,
+    status BOOLEAN,
+    deleted_at DATETIME,
+
+    FOREIGN KEY(cod_cleaner)) REFERENCES clenaer(id),
+    FOREIGN KEY(cod_room)) REFERENCES room(id)
+);
